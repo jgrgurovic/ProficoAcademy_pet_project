e85eba9059a2acb3c3d2c82e1ecf61cf7b96f566
j-grgur-hci <josipa3371@gmail.com>
2023-09-01 00:37:29 +0200

    Homepage basic outline (+Card & Form components)

---
+++ components/Card.tsx
@@ -0,0 +1,17 @@
+
+const Card =()=>{
+    return(
+        <div className="card text-white bg-white bg-opacity-25 ">
+       <img className="card-img-top" src="/bella_fiori_mm.jpg" alt="Card image cap"></img>
+      <div className="card-header">
+        Where is Leigh Occhi?
+      </div>
+      <div className="card-body">
+        <h5 className="card-title ">Bella Fiori</h5>
+        {/* <p className="card-text">Where is Leigh Occhi?</p> */}
+      </div>
+    </div>
+    )
+}
+
+export default Card;
