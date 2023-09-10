8e6e423d88fb6e5a3300eaefa5244f5f36aceea4
j-grgur-hci <josipa3371@gmail.com>
2023-09-10 19:48:17 +0200

    Updated header & footer

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import navTabs from "../../constants/navbar"
+
+const NavigationBar = () => {
+  const pathname = usePathname()
+
+  return (
+    <div>
+      <ul className="hidden sm:flex justify-between space-x-8">
+        {navTabs.map((item, index) => (
+          <li key={index} className="font-courier-prime text-xl text-white">
+            <Link
+              href={item.href}
+              className={`${
+                pathname === item.href ? "text-mainRed" : "hover:text-mainRed"
+              } transition duration-300 ease-in`}>
+              {item.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default NavigationBar
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import Footer from "./Footer"
+import NavigationHeader from "./NavigationHeader"
+import Hero from "./Hero"
+
+export { NavigationHeader, Hero, Footer }
