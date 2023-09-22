45ade1601ed8bb727dff7b33ca30ccfac46721b5
j-grgur-hci <josipa3371@gmail.com>
2023-09-22 02:39:04 +0200

    Fixed folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="relative">
         <NavigationHeader />
         <div className="z-10">{children}</div>
-        <Footer />
+        <Footer/>
       </body>
     </html>
   )
--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-import Footer from "./Footer"
-import Navbar from "./Navbar"
-import Hero from "./Hero"
-
-export { Navbar, Hero, Footer }
